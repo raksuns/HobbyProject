fe85b37b083546450563809c1fc36dfd3c4baa8a
raksuns <raksuns@gmail.com>
2015-04-13 23:28:21 +0900

    adding todo..

---
+++ TodoMVC_Angularjs/js/app.js
@@ -0,0 +1,31 @@
+/**
+ * The main TodoMVC app module
+ *
+ * @type {angular.Module}
+ */
+angular.module('todomvc', ['ngRoute'])
+	.config(function($routeProvider) {
+		'use strict';
+
+		var routeConfig = {
+			controller : 'TodoCtrl',
+			templateUrl : 'todomvc-index.html',
+			resolve : {
+				store : function(todoStorage) {
+					// Get the correct module (API or localStorage).
+					return todoStorage.then(function(module) {
+						// Fetch the todo records in the background.
+						module.get();
+						return module;
+					});
+				}
+			}
+		};
+
+		$routeProvider
+			.when('/', routeConfig)
+			.when('/:status', routeConfig)
+			.otherwise({
+				redirectTo : '/'
+			});
+	});
