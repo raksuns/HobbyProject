a6393bcd5f46debcbc178d6a1c861e49efc8ecfb
raksuns <raksuns@gmail.com>
2015-06-29 15:07:29 +0900

    study ~

--- ProJsTech/Chaptera/03-htmlelement.js
+++ ProJsTech/Chaptera/03-htmlelement.js
@@ -1,5 +1,5 @@
 // Add a new method to all HTML DOM Elements
 // that can be used to see if an Element has a specific class, or not.
-HTMLElement.prototype.hasClass = function( class ) {
-    return new RegExp("(^|\\s)" + class + "(\\s|$)").test( this.className );
+HTMLElement.prototype.hasClass = function(klass ) {
+    return new RegExp("(^|\\s)" + klass + "(\\s|$)").test( this.className );
 };
--- ProJsTech/Chaptera/04-body.js
+++ ProJsTech/Chaptera/04-body.js
@@ -1,5 +1,5 @@
 // Change the margins of the <body>
-document.body.style.margin = “0px”;
+document.body.style.margin = "0px";
 
 // document.body is equivalent to:
-document.getElementsByTagName(“body”)[0]
+document.getElementsByTagName("body")[0];
